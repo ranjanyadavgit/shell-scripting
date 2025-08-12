11351571e76cae2e43ca239e29e3bd10852108ba
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-12 14:32:55 +0530

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "enter a name"
+read name
+
+arg1=$1
+arg2=$2
+
+#print the variable
+
+echo "Your name is: $name"
+echo "first argument is : $arg1"
+echo "second argument is : $arg2"
