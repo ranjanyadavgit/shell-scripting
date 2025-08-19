fd208fac7a800b3c2bcdf5fbbdbef5de271b2b55
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-19 08:19:21 +0000

    Create system_defined.sh

---
+++ system_defined.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#system defined variable
+
+echo $HOME
+echo ${$}
+
+echo $PATH
+echo $PWD
+echo $HOSTNAME
+echo $PPID
+
+sleep 5
+echo $SECONDS
+
+
+output - >
+
+root@ubuntuvm:~# 
+root@ubuntuvm:~# ./user_defined.sh 
+/root
+3517
+/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
+/root
+ubuntuvm
+3274
+5
+root@ubunt
