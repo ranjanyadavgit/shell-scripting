2dd8e0fbfca06ae38c6539fd64b375be040d2cb3
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-19 09:20:20 +0000

    Update and rename commandlineargs.sh to commandlineargs2.sh

---
+++ commandlineargs2.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+
+
+name=${1}
+age=${2}
+
+ echo "hi my name is, ${name} age is ${age}"
+echo ${1}
+echo ${2}
+echo ${3}
+echo ${4}
+echo ${5}
+echo ${6}
+echo ${7}
+echo ${8}
+echo ${9}
+echo ${10}
+echo ${11}
+echo ${12}
+
+o/p ->
+root@ubuntuvm:~# ./commadlineargs.sh "ranjan yadav" 23
+hi my name is, ranjan yadav age is 23
+ranjan yadav
+23
+
+
+
+
+
+
+
+
+
+
+root@ubuntuvm:~# 
+
+
+ranjan
+26
+hi my name is ranjan age is 26
+
