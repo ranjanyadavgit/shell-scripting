6057db8008a4487eed7d77092fa23fd2336564a5
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-19 08:55:18 +0000

    Create commandlineargs.sh

---
+++ commandlineargs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+name="ranjan"
+age="23"
+
+echo ${0}
+echo ${1}
+echo ${2}
+
+echo "hi my name is, ${name} age is ${age}"
+
+o/p - >
+
+root@ubuntuvm:~# ./commadlineargs.sh raj 28
+./commadlineargs.sh
+raj
+28
+hi my name is, ranjan age is 23
+root@ubuntuvm:~# 
