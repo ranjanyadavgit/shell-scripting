d89fe33207f818e12873a44519542144120b7e93
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-22 21:15:03 +0000

    Update string-variable2.sh

--- string-variable2.sh
+++ string-variable2.sh
@@ -8,12 +8,12 @@ echo "${string,,}"
 
 name="my name IS ranjan"
 
-echo "value is ${#name}"
+echo "length of string is ${#name}"
 
 ranjan@ubuntuvm:~$ vim 4.sh
 ranjan@ubuntuvm:~$ ./4.sh
 hi Ranjan, this is my name
 hi ranjan, this is my name
-value is 17
+length of string is 17
 
 
