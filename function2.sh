15547430e2699df308b044e3ca4eda1f02764492
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-25 11:46:05 +0000

    Update function2.sh

--- function2.sh
+++ function2.sh
@@ -25,3 +25,36 @@ ranjan@ubuntuvm:~$ ./func.sh
 install redis nginx
 configure nginx
 deploy app
+--------------------------------------------------------
+
+
+function install(){
+
+echo "install ${2} ${1}"
+echo "${FUNCNAME}"
+}
+
+function configure(){
+
+echo "configure ${1}"
+echo "${FUNCNAME}"
+}
+
+
+function deploy(){
+
+echo "deploy ${1}"
+echo "${FUNCNAME}"
+}
+
+install "nginx" "redis"
+configure "nginx" "redis"
+deploy "app"
+--
+ranjan@ubuntuvm:~$ ./func.sh 
+install redis nginx
+install
+configure nginx
+configure
+deploy app
+deploy
