4fd78bc766dc59460a7e4d656e715e3b8d215f61
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-08-27 08:48:11 +0000

    Create if-condition.sh

---
+++ if-condition.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if echo "hello world"> /dev/null
+then
+ echo "hello world executed successfully"
+fi
+
+echo "hello i am here"
+
+ranjan@ubuntuvm:~$ ./if-condition.sh 
+hello world executed successfully
+hello i am here
