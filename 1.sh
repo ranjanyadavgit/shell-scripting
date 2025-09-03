98c64285e3369f0405d5412bb78a589793d1d10b
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-03 16:59:31 +0000

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -8,18 +8,16 @@ tcp        0      0 ubuntuvm:36560          93.243.107.34.bc.:https ESTABLISHED
 tcp        0      0 ubuntuvm:44956          151.101.1.91:https      ESTABLISHED
 
 ranjan@ubuntuvm:~/Desktop$ netstat | grep -E "http|https"
+ranjan@ubuntuvm:~/Desktop$ netstat -tuln | grep LISTEN
+tcp        0      0 127.0.0.54:53           0.0.0.0:*               LISTEN     
+tcp        0      0 127.0.0.53:53           0.0.0.0:*               LISTEN     
+tcp        0      0 127.0.0.1:631           0.0.0.0:*               LISTEN     
+tcp6       0      0 ::1:631                 :::*                    LISTEN     
+tcp6       0      0 :::80                   :::*                    LISTEN     
+ranjan@ubuntuvm:~/Desktop$ 
 
-tcp        0      0 ubuntuvm:45894          pnmaaa-ap-in-f2.1:https ESTABLISHED
-tcp        0      0 ubuntuvm:35408          sl-in-f119.1e100.:https TIME_WAIT  
-tcp        0      0 ubuntuvm:46942          lcmaaa-ar-in-f10.:https TIME_WAIT  
-tcp        0      0 ubuntuvm:49334          53.253.110.34.bc.:https ESTABLISHED
-tcp        0      0 ubuntuvm:34488          123.208.120.34.bc:https ESTABLISHED
-tcp        0      0 ubuntuvm:53650          ams17s02-in-f8.1e:https TIME_WAIT  
-tcp        0      0 ubuntuvm:50450          maa05s20-in-f3.1e:https TIME_WAIT  
-tcp        0      0 ubuntuvm:38994          lb-140-82-114-26-:https ESTABLISHED
-tcp        0      0 ubuntuvm:50466          maa05s20-in-f3.1e:https TIME_WAIT  
-tcp        0      0 ubuntuvm:53642          ams17s02-in-f8.1e:https TIME_WAIT  
-tcp        0      0 ubuntuvm:50884          76.237.120.34.bc.:https ESTABLISHED
+
+
 tcp        0      0 ubuntuvm:35394          sl-in-f119.1e100.:https TIME_WAIT  
 tcp        0      0 ubuntuvm:38352          151.101.65.91:https     ESTABLISHED
 tcp        0      0 ubuntuvm:55702          pnmaaa-bb-in-f14.:https ESTABLISHED
