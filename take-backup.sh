4a6b2f6651d206793c78130a26d57a05fbcd98dd
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-03 21:51:52 +0530

    Update take-backup.sh

--- take-backup.sh
+++ take-backup.sh
@@ -8,3 +8,14 @@ mkdir -p $DESTINATION/$DATE
 cp -r $SOURCE $DESTINATION/$DATE
 
 echo "Backup completed on $DATE"
+
+
+#Explanation
+#• SOURCE: The directory to be backed up.
+#• DESTINATION: The directory where the backup will be stored.
+#• DATE: Captures the current date and time to create a unique backup folder.
+#• mkdir -p $DESTINATION/$DATE: Creates the backup directory if it does not exist.
+#• cp -r $SOURCE $DESTINATION/$DATE: Copies the contents of the source directory
+#to the backup directory.
+#• echo "Backup completed on $DATE": Outputs a message indicating the completion
+#of the backup.
