1ec0f7b5c9a65ddba0a731aebe658f9ae2f09ce4
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-03 16:48:20 +0000

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,3 +2,7 @@
 
 echo "hello world"
 
+ranjan@ubuntuvm:~/Desktop$ netstat | grep -E "http|https"
+tcp        0      0 ubuntuvm:38994          lb-140-82-114-26-:https ESTABLISHED
+tcp        0      0 ubuntuvm:36560          93.243.107.34.bc.:https ESTABLISHED
+tcp        0      0 ubuntuvm:44956          151.101.1.91:https      ESTABLISHED
